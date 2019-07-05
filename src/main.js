473f87dc270544c0af1d3515ada6ec79ab9174fc
Priyo Ardi Nugroho 73 <priyokan316@gmail.com>
2019-07-05 07:55:51 +0700

    installing vue-material

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(MdButton)
+Vue.use(MdContent)
+Vue.use(MdTabs)
 
 Vue.config.productionTip = false
 
